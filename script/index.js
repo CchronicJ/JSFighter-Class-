ba919ffca857abd24376de4446bfa3879dc5c526
Caleb <caleb2021013@live.ytech.edu>
2019-11-12 08:42:56 -0500

    t

--- script/index.js
+++ script/index.js
@@ -1,16 +1,33 @@
-function attack() {
-  // This is how we write to developer console
-  // console.log("Rawr! I have attacked!");
+class Fighter {
+  constructor(name, charaname) {
+     this.name = name;
+     this.hp = 40;
+     this.sp = 20;
+     this.atk = 10;
+     this.def = 10;
+     this.tek = 10;
+     this.charaname = charaname;
+  }
+  attack(target) {
+    console.log(this.name + "Attacked!" + target.name);
+  }
+  single(target) {
+    this.attack(target);
+  }
+  double(target) {
+    this.attack(target);
+    this.attack(target);
+  }
+  recover() {
+    console.log("Recovered!");
+  }
+}
+let Player0;
+let Player1;
+function startup() {
+let Player0 = new Fighter("Crash", "crashr");
+let Player1 = new Fighter("Sam", "saml");
 
-  // This is how we get the content of 'outputBox'
-  // console.log(document.getElementById('outputBox').innerHTML)
-
-  // This is how we change 'outputBox's innerHTML
-  // document.getElementById('outputBox').innerHTML = "New Text <br>"
-
-  let oldText = document.getElementById('outputBox').innerHTML
-  document.getElementById('outputBox').innerHTML = "Attacked!<br>"
-  document.getElementById('outputBox').innerHTML += "Attacked again!<br>"
-  document.getElementById('outputBox').innerHTML += oldText
-
+  console.log("My name is " + Player0.name + " and my ATK is " + Player0.atk);
+    console.log("My name is " + Player1.name + " and my ATK is " + Player1.atk);
 }
